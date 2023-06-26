54cc508489af7711f82b148dcce5cd087040c2e9
Bùi Đức Tuấn <buiductuan12081995@gmail.com>
2023-06-26 21:01:30 +0700

    tesst

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "discipline",
+    'name': "Discipline",
 
     'summary': """Quy trình xử lý học vụ của sinh viên dành cho 1 khoa hoặc nhiều khoa""",
 
@@ -8,13 +8,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "TT",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Odoo core/odoo core',
+    'category': 'Odoo Basic/Odoo Basic',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -22,9 +22,9 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/major_view.xml',
     ],
     # only loaded in demonstration mode
-    "application": True
+    "application": True,
+    'installable':True,
 }
